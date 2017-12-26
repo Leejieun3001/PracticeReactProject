5f3c76c2d4b95ef25e3fa7f9cf2bb233ac3693b5
DESKTOP-V0ENI3K\LG <jieun3001@gmail.com>
2017-12-26 16:16:44 +0900

    함수  bind 3 (이벤트 처리)

--- React05/src/components/App.js
+++ React05/src/components/App.js
@@ -8,15 +8,21 @@ class App extends React.Component {
 constructor(){
     super();
     this.state = {
-        data: '여기에 데이터를 입력하세요'
+        data: '여기에 데이터를 입력하세요',
+        data2 : 'init data'
     }
     //함수 bind
     this.changed = this.changed.bind(this);
+    this.updateHandler = this.updateHandler.bind(this);
 }
 
 //변수를 바꿔주는 함수
 changed(e){
     this.setState({data : e.target.value});
+}
+
+updateHandler(){
+    this.setState({data2: "updata data"});
 }
 
     render(){
@@ -26,7 +32,21 @@ changed(e){
                 <input type = "text" onChange={this.changed} value = {this.state.data}/><br/>
 
                 <h1>입력 결과 : {this.state.data} </h1>
-                </div>
+                <br/>
+                <Content mProp = {this.state.data2} updateProp = {this.updateHandler} />     
+            </div>
+        );
+    }
+}
+
+//Content 컴포넌트 생성
+class Content extends React.Component{
+    render(){
+        return(
+            <div>
+                <button onClick = {this.props.updateProp} > BUTTON </button>
+                <h1> {this.props.mProp} </h1>
+            </div>
         );
     }
 }
